70ad9bca5d2239afe85c2b11e5bc724d39597d9d
Boss <bossnathakorn@gmail.com>
2024-07-26 16:45:22 +0700

    add basic compile code

--- crates/vnsl_compiler/src/lib.rs
+++ crates/vnsl_compiler/src/lib.rs
@@ -1,13 +1,30 @@
-use anyhow::Ok;
+use std::io;
+
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use vnsl_core::model::VnslScene;
 
 mod fs;
 
-pub fn compile_dir(path: &str) -> anyhow::Result<()> {
-    fs::scan_scripts(path, true)?;
-    Ok(())
+pub fn compile_dir(path: &str) -> anyhow::Result<Vec<VnslScene>> {
+    let scripts: io::Result<Vec<String>> = fs::scan_scripts(path, true)?
+        .iter()
+        .map(|p| std::fs::read_to_string(p))
+        .collect();
+    scripts?.par_iter().map(|src| compile(src)).collect()
 }
 
-pub fn compile(script: &str) -> anyhow::Result<()> {
-    let _ = vnsl_parser::parse(script).expect("Success");
-    Ok(())
+pub fn compile(script: &str) -> anyhow::Result<VnslScene> {
+    vnsl_parser::parse(script)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::compile_dir;
+
+    #[test]
+    fn test_compile_dir() -> anyhow::Result<()> {
+        let result = compile_dir("./test_scripts")?;
+        println!("{:#?}", result);
+        Ok(())
+    }
 }
