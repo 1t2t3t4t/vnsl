54c0474de1871d8ad5846e80f20a9cd9efe4bd1a
Boss <bossnathakorn@gmail.com>
2024-07-26 16:45:09 +0700

    fix bug parsing number

--- crates/vnsl_parser/src/data_type.rs
+++ crates/vnsl_parser/src/data_type.rs
@@ -1,7 +1,14 @@
 use crate::Rule;
-use anyhow::Ok;
+use anyhow::{Error, Ok};
 use pest::iterators::Pair;
+use thiserror::Error;
 use vnsl_core::model::VnslDataType;
+
+#[derive(Debug, Error)]
+pub enum ParseDataTypeError {
+    #[error("Cannot parse number {0} with error {1}")]
+    CannotParseNumber(String, Error),
+}
 
 pub fn parse_data_type(rule: Pair<Rule>) -> anyhow::Result<VnslDataType> {
     let mut inner = rule.into_inner();
@@ -10,8 +17,16 @@ pub fn parse_data_type(rule: Pair<Rule>) -> anyhow::Result<VnslDataType> {
     match inner.as_rule() {
         Rule::string => Ok(VnslDataType::String(parse_string(inner))),
         Rule::number => {
-            let num = inner.as_str().parse::<f64>()?;
-            Ok(VnslDataType::Number(num))
+            let num_str = inner.as_str();
+            let num = match num_str.trim().parse::<f64>() {
+                std::result::Result::Ok(double) => Ok(double),
+                Err(err) => Err(ParseDataTypeError::CannotParseNumber(
+                    num_str.to_string(),
+                    err.into(),
+                )
+                .into()),
+            };
+            Ok(VnslDataType::Number(num?))
         }
         Rule::bool => {
             let bool_val = inner.as_str() == "true";
