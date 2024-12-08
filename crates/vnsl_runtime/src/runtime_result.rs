186692f71ad962cbcd26fc866bb591f9a68e6ec9
Boss <bossnathakorn@gmail.com>
2024-12-08 16:48:18 +0700

    update runtime error format

--- crates/vnsl_runtime/src/runtime_result.rs
+++ crates/vnsl_runtime/src/runtime_result.rs
@@ -1,4 +1,5 @@
-#[derive(Debug)]
+use std::fmt::Debug;
+
 pub enum RuntimeError {
     NoSceneLoaded,
     LuaEvalError(String, mlua::Error),
@@ -10,5 +11,18 @@ pub type RuntimeResult<T> = Result<T, RuntimeError>;
 impl From<mlua::Error> for RuntimeError {
     fn from(value: mlua::Error) -> Self {
         Self::LuaError(value)
+    }
+}
+
+impl Debug for RuntimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RuntimeError::NoSceneLoaded => writeln!(f, "No scene is loaded for current runtime"),
+            RuntimeError::LuaEvalError(line, error) => {
+                writeln!(f, "Lua eval error for line \"{}\"", line)?;
+                writeln!(f, "{:#?}", error)
+            }
+            RuntimeError::LuaError(error) => writeln!(f, "Lua runtime error {:#?}", error),
+        }
     }
 }
