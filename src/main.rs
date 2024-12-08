4d80352685280b331e9e393e8364ddce2316282e
Boss <bossnathakorn@gmail.com>
2024-12-08 17:51:55 +0700

    rm unused code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use vnsl_runtime::RuntimeDelegateHandler;
-
-struct Handler;
-
-impl RuntimeDelegateHandler for Handler {}
-
 fn main() -> anyhow::Result<()> {
     let test_script = include_str!("../spec.vnsl");
     let scene = vnsl_compiler::compile(test_script)?;
 
     let mut runtime = vnsl_runtime::Runtime::new();
     runtime.load_scene(scene);
-    let handler = Handler;
 
     loop {
-        let res = runtime.step(&handler).unwrap();
+        let res = runtime.step().unwrap();
         println!("{:#?}", res);
 
         if matches!(res, vnsl_runtime::RuntimeCommand::EndOfScene) {
