06a2f93ba5b01a9756028c87c3d56d068eac0e21
Boss <bossnathakorn@gmail.com>
2024-12-09 13:39:48 +0700

    add elapsed test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use std::time;
+
+fn elapsed<T>(f: impl FnOnce() -> T) -> T {
+    let start = time::Instant::now();
+    let res = f();
+    println!("Elapsed time: {} us.", start.elapsed().as_micros());
+    res
+}
+
 fn main() -> anyhow::Result<()> {
     let test_script = include_str!("../spec.vnsl");
-    let scene = vnsl_compiler::compile(test_script)?;
+    let scene = elapsed(|| vnsl_compiler::compile(test_script))?;
 
     let mut runtime = vnsl_runtime::Runtime::new();
     runtime.load_scene(scene);
 
     loop {
         let res = runtime.step().unwrap();
-        println!("{:#?}", res);
+        println!("{:?}", res);
 
         if matches!(res, vnsl_runtime::RuntimeCommand::EndOfScene) {
             break;
