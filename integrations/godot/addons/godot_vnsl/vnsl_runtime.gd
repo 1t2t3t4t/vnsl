af56191099101c57126ac4e8c0b9ebab80a8d306
Boss <bossnathakorn@gmail.com>
2025-02-01 22:01:22 +0700

    rename var

--- integrations/godot/addons/godot_vnsl/vnsl_runtime.gd
+++ integrations/godot/addons/godot_vnsl/vnsl_runtime.gd
@@ -5,8 +5,8 @@ class_name VnslRuntime
 @export var scripts_path: String = "res://scripts/"
 
 func _ready() -> void:
-	var result := _scan_scripts(scripts_path)
-	construct_scene_map(result)
+	var scripts := _scan_scripts(scripts_path)
+	construct_scene_map(scripts)
 	var res := load_scene("MainScene")
 	if res.is_err():
 		print(res.err_message())
