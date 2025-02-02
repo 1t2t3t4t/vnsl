37b9bbd2fce432f99b9f371c0661212ce8df7c8f
Boss <bossnathakorn@gmail.com>
2025-02-03 02:46:36 +0700

    clean lua expr string

--- crates/vnsl_parser/src/statement/lua_expr.rs
+++ crates/vnsl_parser/src/statement/lua_expr.rs
@@ -7,6 +7,10 @@ use crate::{utils, Rule};
 pub fn parse_lua_expr(rule: Pair<Rule>) -> Result<VnslLuaExpr> {
     let lua = utils::extract_inner_as_rule(rule, Rule::lua_script)
         .as_str()
-        .to_string();
+        .to_string()
+        .split("\n")
+        .map(|s| s.trim().to_string())
+        .collect::<Vec<String>>()
+        .join("\n");
     Ok(VnslLuaExpr { lua })
 }
